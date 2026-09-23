9567049fb31afeebc4ee5b096d8248e583b06827
Anurag Basuri <anuragbasuri@gmail.com>
2026-09-23 15:19:31 +0530

    first commit

---
+++ smart-desk-booking/src/main/java/com/anurag/smartdesk/SmartDeskBookingApplication.java
@@ -0,0 +1,13 @@
+package com.anurag.smartdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDeskBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDeskBookingApplication.class, args);
+	}
+
+}
---
+++ smart-desk-booking/src/test/java/com/anurag/smartdesk/SmartDeskBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.smartdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDeskBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
